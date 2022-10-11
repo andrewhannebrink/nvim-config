8cd0310043cb69955ee54d7038ad74f5bb9d9bf9
andrewhannebrink <ahannebrink@safelease.com>
2022-10-11 16:32:52 -0500

    commented out auto formatting and mapped enter to cmp.confirm()

--- plugin/lspconfig.rc.lua
+++ plugin/lspconfig.rc.lua
@@ -5,12 +5,12 @@ local protocol = require('vim.lsp.protocol')
 
 local on_attach = function(client, bufnr)
   -- formatting
-  if client.server_capabilities.documentFormattingProvider then
-    vim.api.nvim_command [[augroup Format]]
-    vim.api.nvim_command [[autocmd! * <buffer>]]
-    vim.api.nvim_command [[autocmd BufWritePre <buffer> lua vim.lsp.buf.formatting_seq_sync()]]
-    vim.api.nvim_command [[augroup END]]
-  end
+  --  if client.server_capabilities.documentFormattingProvider then
+  --    vim.api.nvim_command [[augroup Format]]
+  --    vim.api.nvim_command [[autocmd! * <buffer>]]
+  --    vim.api.nvim_command [[autocmd BufWritePre <buffer> lua vim.lsp.buf.formatting_seq_sync()]]
+  --    vim.api.nvim_command [[augroup END]]
+  --  end
 end
 
 nvim_lsp.tsserver.setup {
