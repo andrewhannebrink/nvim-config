17ea00bcde81314b4d766a00eaac4962f6f1777d
Andrew Hannebrink <andrewhannebrink@Andrews-MacBook-Pro.local>
2024-07-22 20:51:03 -0500

    conceptual setup.sh

---
+++ setup.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+# Copy the files to the correct locations
+# This has never been tested
+
+cp ./DefaultKeyBinding.dict ~/Library/KeyBindings/
+cp ./arrow\ keys.dict ~/Library/Keyboard\ Layouts/
+cp ./.tmux.conf ~/
+cp ./.zshrc ~/
+cp -r alacritty ~/.config/
+cp -r nvim ~/.config/
+cp -r yabai ~/.config/
+cp -r skhd ~/.config/
+cp -r karabiner ~/.config/
