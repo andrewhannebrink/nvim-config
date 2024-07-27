cd120eede90597335667345c0be451d3870e3b49
Andrew Hannebrink <andrewhannebrink@Andrews-MacBook-Pro.local>
2024-07-26 19:11:09 -0500

    alpha dash

---
+++ nvim/lua/plugins/alpha.lua
@@ -0,0 +1,44 @@
+return {
+  'goolord/alpha-nvim',
+  config = function ()
+    local alpha = require"alpha"
+    local dashboard = require'alpha.themes.dashboard'
+    dashboard.section.header.val = {
+      "",
+      " ⣿⣿⣷⡁⢆⠈⠕⢕⢂⢕⢂⢕⢂⢔⢂⢕⢄⠂⣂⠂⠆⢂⢕⢂⢕⢂⢕⢂⢕⢂ ",
+      " ⣿⣿⣿⡷⠊⡢⡹⣦⡑⢂⢕⢂⢕⢂⢕⢂⠕⠔⠌⠝⠛⠶⠶⢶⣦⣄⢂⢕⢂⢕ ",
+      " ⣿⣿⠏⣠⣾⣦⡐⢌⢿⣷⣦⣅⡑⠕⠡⠐⢿⠿⣛⠟⠛⠛⠛⠛⠡⢷⡈⢂⢕⢂ ",
+      " ⠟⣡⣾⣿⣿⣿⣿⣦⣑⠝⢿⣿⣿⣿⣿⣿⡵⢁⣤⣶⣶⣿⢿⢿⢿⡟⢻⣤⢑⢂ ",
+      " ⣾⣿⣿⡿⢟⣛⣻⣿⣿⣿⣦⣬⣙⣻⣿⣿⣷⣿⣿⢟⢝⢕⢕⢕⢕⢽⣿⣿⣷⣔ ",
+      " ⣿⣿⠵⠚⠉⢀⣀⣀⣈⣿⣿⣿⣿⣿⣿⣿⣿⣿⣗⢕⢕⢕⢕⢕⢕⣽⣿⣿⣿⣿ ",
+      " ⢷⣂⣠⣴⣾⡿⡿⡻⡻⣿⣿⣴⣿⣿⣿⣿⣿⣿⣷⣵⣵⣵⣷⣿⣿⣿⣿⣿⣿⡿ ",
+      " ⢌⠻⣿⡿⡫⡪⡪⡪⡪⣺⣿⣿⣿⣿⣿⠿⠿⢿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠃ ",
+      " ⠣⡁⠹⡪⡪⡪⡪⣪⣾⣿⣿⣿⣿⠋⠐⢉⢍⢄⢌⠻⣿⣿⣿⣿⣿⣿⣿⣿⠏⠈ ",
+      " ⡣⡘⢄⠙⣾⣾⣾⣿⣿⣿⣿⣿⣿⡀⢐⢕⢕⢕⢕⢕⡘⣿⣿⣿⣿⣿⣿⠏⠠⠈ ",
+      " ⠌⢊⢂⢣⠹⣿⣿⣿⣿⣿⣿⣿⣿⣧⢐⢕⢕⢕⢕⢕⢅⣿⣿⣿⣿⡿⢋⢜⠠⠈ ",
+      " ⠄⠁⠕⢝⡢⠈⠻⣿⣿⣿⣿⣿⣿⣿⣷⣕⣑⣑⣑⣵⣿⣿⣿⡿⢋⢔⢕⣿⠠⠈ ",
+      " ⠨⡂⡀⢑⢕⡅⠂⠄⠉⠛⠻⠿⢿⣿⣿⣿⣿⣿⣿⣿⣿⡿⢋⢔⢕⢕⣿⣿⠠⠈ ",
+      " ⠄⠪⣂⠁⢕⠆⠄⠂⠄⠁⡀⠂⡀⠄⢈⠉⢍⢛⢛⢛⢋⢔⢕⢕⢕⣽⣿⣿⠠⠈ ",
+    }
+    dashboard.section.buttons.val = {
+      --dashboard.button("r", "󰂣  Recent", "<cmd> Telescope oldfiles <cr>"),
+      dashboard.button("r", "󰊄  Recently opened files", "<cmd> Telescope oldfiles <cr>"),
+      --dashboard.button("q", "󰂣  Just", "<cmd> <cr>"),
+      --dashboard.button("q", "󰂣  Pitch", "<cmd> <cr>"),
+      --dashboard.button("q", "󰂣  (Never) Quit", "<cmd> <cr>"),
+      --dashboard.button("s", " Restore", [[<cmd> lua require("persistence").load() <cr>]]),
+    } 
+
+    --button("e", "  New file", "<cmd>ene <CR>"),
+    --button("SPC f f", "󰈞  Find file"),
+    --button("SPC f h", "󰊄  Recently opened files"),
+    --button("SPC f r", "  Frecency/MRU"),
+    --button("SPC f g", "󰈬  Find word"),
+    --button("SPC f m", "  Jump to bookmarks"),
+    --button("SPC s l", "  Open last session"),
+
+    dashboard.section.footer.val = "\"Just pitch\""
+
+    alpha.setup(dashboard.opts)
+  end
+};
